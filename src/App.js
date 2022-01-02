6e9aafa7b0f6e8f493a92ddb1ead0e7dcdee2b78
Juan <morenostudent1@gmail.com>
2022-01-02 09:36:38 -0800

    adding components

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+
+        <main className='container mx-auto px-3 pb-12'>
+          {/* Routes wrapped in <Routes> */}
+          <Routes>
+            {/* Home route */}
+            <Route path='/' element={<Home />}></Route>
+            {/* About route */}
+            <Route path='/about' element={<About />}></Route>
+            {/* Not Found route (to send - optional) */}
+            <Route path='/notfound' element={<NotFound />}></Route>
+            {/* Not Found (404) route */}
+            <Route path='/*' element={<NotFound />}></Route>
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+      </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <h1 className='text-6xl'>
+            Hello
+        </h1>
+    )
+}
+
+export default Home
